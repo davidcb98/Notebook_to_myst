15368f3c22586fe3c116b071322a3559d384ade2
davidcb98 <davidcb1698@gmail.com>
2024-02-29 12:59:45 +0100

    Mejoras build_in_book_branch, v7

--- scripts/Build_in_book_branch.sh
+++ scripts/Build_in_book_branch.sh
@@ -2,65 +2,52 @@ rama_origen=$(git rev-parse --abbrev-ref HEAD)
 
 git diff --quiet
 error=$?
-if ! [ $error == 0 ] ; then
-    echo "ERROR. Ejecución abortada. Estás en la rama: " `git rev-parse --abbrev-ref HEAD`
-    exit
-fi
+if ! [ $error == 0 ] ; then echo "ERROR. Ejecución abortada. Estás en la rama: " `git rev-parse --abbrev-ref HEAD`;  exit; fi
 
 
 git checkout book_branch
 error=$?
-if ! [ $error == 0 ] ; then
-    echo "ERROR: falta hacer un commit. Ejecución abortada. Estás en la rama: " `git rev-parse --abbrev-ref HEAD`
-    exit
+if ! [ $error == 0 ] ; then echo "ERROR: falta hacer un commit. Ejecución abortada. Estás en la rama: " `git rev-parse --abbrev-ref HEAD`; exit
+fi
+
+git checkout -B temp_branch book_branch
+error=$?
+if ! [ $error == 0 ] ; then echo "ERROR: falta hacer un commit. Ejecución abortada. Estás en la rama: " `git rev-parse --abbrev-ref HEAD`; exit
+fi
+
+git checkout book_branch
+error=$?
+if ! [ $error == 0 ] ; then echo "ERROR: falta hacer un commit. Ejecución abortada. Estás en la rama: " `git rev-parse --abbrev-ref HEAD`; exit
 fi
 
 git reset --hard $rama_origen
 error=$?
-if ! [ $error == 0 ] ; then
-    echo "ERROR. Ejecución abortada. Estás en la rama: " `git rev-parse --abbrev-ref HEAD`
-    exit
+if ! [ $error == 0 ] ; then echo "ERROR: falta hacer un commit. Ejecución abortada. Estás en la rama: " `git rev-parse --abbrev-ref HEAD`; exit
 fi
+
+git checkout temp_branch -- ../Book/_build
 
 ./Notebook_to_myst.sh
 error=$?
-if ! [ $error == 0 ] ; then
-    echo "ERROR. Ejecución abortada. Estás en la rama: " `git rev-parse --abbrev-ref HEAD`
-    exit
+if ! [ $error == 0 ] ; then echo "ERROR: falta hacer un commit. Ejecución abortada. Estás en la rama: " `git rev-parse --abbrev-ref HEAD`; exit
 fi
 
 git add ../
-error=$?
-if ! [ $error == 0 ] ; then
-    echo "ERROR. Ejecución abortada. Estás en la rama: " `git rev-parse --abbrev-ref HEAD`
-    exit
-fi
 
 git commit -m "Compilamos book"
-error=$?
-if ! [ $error == 0 ] ; then
-    echo "ERROR. Ejecución abortada. Estás en la rama: " `git rev-parse --abbrev-ref HEAD`
-    exit
-fi
 
 git pull -s recursive -X ours origin book_branch --no-edit
 error=$?
-if ! [ $error == 0 ] ; then
-    echo "ERROR. Ejecución abortada. Estás en la rama: " `git rev-parse --abbrev-ref HEAD`
-    exit
+if ! [ $error == 0 ] ; then echo "ERROR: falta hacer un commit. Ejecución abortada. Estás en la rama: " `git rev-parse --abbrev-ref HEAD`; exit
 fi
 
 
 git push origin book_branch
 error=$?
-if ! [ $error == 0 ] ; then
-    echo "ERROR. Ejecución abortada. Estás en la rama: " `git rev-parse --abbrev-ref HEAD`
-    exit
+if ! [ $error == 0 ] ; then echo "ERROR: falta hacer un commit. Ejecución abortada. Estás en la rama: " `git rev-parse --abbrev-ref HEAD`; exit
 fi
 
 git checkout $rama_origen
 error=$?
-if ! [ $error == 0 ] ; then
-    echo "ERROR. Ejecución abortada. Estás en la rama: " `git rev-parse --abbrev-ref HEAD`
-    exit
+if ! [ $error == 0 ] ; then echo "ERROR: falta hacer un commit. Ejecución abortada. Estás en la rama: " `git rev-parse --abbrev-ref HEAD`; exit
 fi
