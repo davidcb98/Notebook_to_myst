d2b3e8fe8021b24f289b4506908a51849643a435
davidcb98 <davidcb1698@gmail.com>
2024-02-29 12:27:10 +0100

    Mejoras build_in_book_branch, v3

--- scripts/Build_in_book_branch.sh
+++ scripts/Build_in_book_branch.sh
@@ -1,46 +1,52 @@
 rama_origen=$(git rev-parse --abbrev-ref HEAD)
 
-git status | egrep "l árbol de trabajo está limpio | working tree is clean"
+git diff --quiet
+error=$?
+if ! [ $error == 0 ] ; then
+    echo "ERROR. Ejecución abortada. Estás en la rama: " `git rev-parse --abbrev-ref HEAD`
+    exit
+fi
 
 
 git checkout book_branch
 error=$?
 if ! [ $error == 0 ] ; then
-    echo "Estás en la rama: " `git rev-parse --abbrev-ref HEAD`
+    echo "ERROR. Ejecución abortada. Estás en la rama: " `git rev-parse --abbrev-ref HEAD`
     exit
 fi
 
 git reset --hard $rama_origen
 error=$?
-if ! [ $error == 0 ] ;
-    echo "Estás en la rama: " `git rev-parse --abbrev-ref HEAD`
+if ! [ $error == 0 ] ; then
+    echo "ERROR. Ejecución abortada. Estás en la rama: " `git rev-parse --abbrev-ref HEAD`
     exit
 fi
 
 ./Notebook_to_myst.sh
 error=$?
 if ! [ $error == 0 ] ; then
-    echo "Estás en la rama: " `git rev-parse --abbrev-ref HEAD`
+    echo "ERROR. Ejecución abortada. Estás en la rama: " `git rev-parse --abbrev-ref HEAD`
     exit
 fi
 
 git add ../
 error=$?
 if ! [ $error == 0 ] ; then
-    echo "Estás en la rama: " `git rev-parse --abbrev-ref HEAD`
+    echo "ERROR. Ejecución abortada. Estás en la rama: " `git rev-parse --abbrev-ref HEAD`
     exit
 fi
 
 git commit -m "Compilamos book"
 error=$?
 if ! [ $error == 0 ] ; then
-    echo "Estás en la rama: " `git rev-parse --abbrev-ref HEAD`
+    echo "ERROR. Ejecución abortada. Estás en la rama: " `git rev-parse --abbrev-ref HEAD`
     exit
 fi
 
 git pull -s recursive -X ours origin book_branck
+error=$?
 if ! [ $error == 0 ] ; then
-    echo "Estás en la rama: " `git rev-parse --abbrev-ref HEAD`
+    echo "ERROR. Ejecución abortada. Estás en la rama: " `git rev-parse --abbrev-ref HEAD`
     exit
 fi
 
@@ -48,13 +54,13 @@ fi
 git push origin book_branch
 error=$?
 if ! [ $error == 0 ] ; then
-    echo "Estás en la rama: " `git rev-parse --abbrev-ref HEAD`
+    echo "ERROR. Ejecución abortada. Estás en la rama: " `git rev-parse --abbrev-ref HEAD`
     exit
 fi
 
 git checkout $rama_origen
 error=$?
 if ! [ $error == 0 ] ; then
-    echo "Estás en la rama: " `git rev-parse --abbrev-ref HEAD`
+    echo "ERROR. Ejecución abortada. Estás en la rama: " `git rev-parse --abbrev-ref HEAD`
     exit
 fi
