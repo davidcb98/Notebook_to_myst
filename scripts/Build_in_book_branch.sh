7bbfe3b52be27e028e6265d1222a033c80e56380
davidcb98 <davidcb1698@gmail.com>
2024-02-29 12:21:29 +0100

    Mejoras build_in_book_branch

--- scripts/Build_in_book_branch.sh
+++ scripts/Build_in_book_branch.sh
@@ -3,41 +3,55 @@ rama_origen=$(git rev-parse --abbrev-ref HEAD)
 git checkout book_branch
 error=$?
 if ! [ $error == 0 ] ; then
+    echo "Estás en la rama: " `git rev-parse --abbrev-ref HEAD`
     exit
 fi
 
 git reset --hard $rama_origen
 error=$?
-if ! [ $error == 0 ] ; then
+if ! [ $error == 0 ] ;
+    echo "Estás en la rama: " `git rev-parse --abbrev-ref HEAD`
     exit
 fi
 
 ./Notebook_to_myst.sh
 error=$?
 if ! [ $error == 0 ] ; then
+    echo "Estás en la rama: " `git rev-parse --abbrev-ref HEAD`
     exit
 fi
 
 git add ../
 error=$?
 if ! [ $error == 0 ] ; then
+    echo "Estás en la rama: " `git rev-parse --abbrev-ref HEAD`
     exit
 fi
 
 git commit -m "Compilamos book"
 error=$?
 if ! [ $error == 0 ] ; then
+    echo "Estás en la rama: " `git rev-parse --abbrev-ref HEAD`
     exit
 fi
+
+git pull -s recursive -X ours origin book_branck
+if ! [ $error == 0 ] ; then
+    echo "Estás en la rama: " `git rev-parse --abbrev-ref HEAD`
+    exit
+fi
+
 
 git push origin book_branch
 error=$?
 if ! [ $error == 0 ] ; then
+    echo "Estás en la rama: " `git rev-parse --abbrev-ref HEAD`
     exit
 fi
 
 git checkout $rama_origen
 error=$?
 if ! [ $error == 0 ] ; then
+    echo "Estás en la rama: " `git rev-parse --abbrev-ref HEAD`
     exit
 fi
